77371e485c96c0c3c40a82158acf98fd7d3f5a76
Bogdan Finn <bogdan.finn@googlemail.com>
2022-09-28 00:40:58 +0200

    updated prebuild binaries;

--- cffi_dist/dist/tls-client-darwin-arm64-0.6.0.h
+++ cffi_dist/dist/tls-client-darwin-arm64-0.6.0.h
@@ -68,6 +68,7 @@ typedef struct { void *data; GoInt len; GoInt cap; } GoSlice;
 extern "C" {
 #endif
 
+extern char* getCookiesFromSession(char* getCookiesParams);
 extern char* request(char* requestParams);
 
 #ifdef __cplusplus
--- cffi_dist/dist/tls-client-windows-64-0.6.0.h
+++ cffi_dist/dist/tls-client-windows-64-0.6.0.h
@@ -68,6 +68,7 @@ typedef struct { void *data; GoInt len; GoInt cap; } GoSlice;
 extern "C" {
 #endif
 
+extern __declspec(dllexport) char* getCookiesFromSession(char* getCookiesParams);
 extern __declspec(dllexport) char* request(char* requestParams);
 
 #ifdef __cplusplus
